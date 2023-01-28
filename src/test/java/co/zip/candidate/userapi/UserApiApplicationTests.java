f58d2f77515d34468e0ab0f0bbce5b887f6495c3
leonandhard <ruoxi.bao96@gmail.com>
2023-01-28 11:18:11 +1100

    Add initial repository

---
+++ src/test/java/co/zip/candidate/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.zip.candidate.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
